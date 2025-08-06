601d568f6fcbf86bc0f714d38d99fada278efdb5
Christina Burky <chrpeebles@gmail.com>
2025-08-06 14:54:32 -0700

    Updates last name and styling

--- index.js
+++ index.js
@@ -1,9 +1,2 @@
-// copyright year
-const copyRightYear = document.getElementById('copyright-year');
-
+const copyRightYear = document.getElementById("copyright-year");
 copyRightYear.innerHTML = new Date().getFullYear();
-
-// fade scroll
-$(window).scroll(function () {
-  $('.top').css('opacity', 1 - $(window).scrollTop() / 300);
-});
